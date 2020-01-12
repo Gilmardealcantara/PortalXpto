24f624577400e4015c6abd4d8d525448665122d6
Gilmar <gilmar.alcantara@ibm.com>
2020-01-12 03:52:48 -0300

    Remove margins

--- xpto-portal/src/components/Header/index.tsx
+++ xpto-portal/src/components/Header/index.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core';
 
-const Header: React.FC = () => <div>Header</div>;
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      display: 'flex',
+      flex: 1,
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100vw',
+      height: '50px',
+      backgroundColor: 'grey',
+      paddingTop: 0,
+      marginTop: 0,
+    },
+  }),
+);
+
+const Header: React.FC = () => {
+  const classes = useStyles();
+
+  return <div className={classes.container}>Header</div>;
+};
 
 export default Header;
