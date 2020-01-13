9b3a5041d278fbb7cf9e71505a59e5c89f5f6657
Gilmar <gilmar.alcantara@ibm.com>
2020-01-13 01:00:54 -0300

    check permission in app 2

---
+++ App1/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace App1.Controllers
+{
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+        public IActionResult Index(string token)
+        {
+            Console.WriteLine("\n\nCHECK_TOKEN");
+            Console.WriteLine(token);
+            Console.WriteLine("\n\n");
+            if (token != null)
+                return Ok();
+            return Unauthorized();
+        }
+    }
+}
